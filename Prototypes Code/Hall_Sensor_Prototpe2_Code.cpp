954184e77e9feb62ee74e01cdac005f66831221f
cgo22 <cgo22@case.edu>
2026-03-16 01:57:59 -0400

    Wrote some mode code for hall effect

--- Prototypes Code/Hall_Sensor_Prototpe2_Code.cpp
+++ Prototypes Code/Hall_Sensor_Prototpe2_Code.cpp
@@ -1,11 +1,25 @@
 /** Name of Author: Gerald Onyedika
  * Course: ECSE 395
- * Project Name: Hall Effect Sensor Code for Prototype 2 */ 
+ * Project Name: Hall Effect Sensor Code for Prototype 2
+ * This is just a prototype code, it's not meant to account for edge cases
+ *  */ 
 
  #include <Arduino.h>
 
  const int hallEffect1 = 0; // Assigning the first hall effect sensor to a pin
  const int  hallEffect2=1; // Assigning the second hall effect sensor to a pin 
+int numOfPillsDispensed =0; // Starts at Zero
+int numOfPillsInContainer = 100; // Defined by the user or using volume calcuations
+int numberOfPillsLeft = numOfPillsInContainer - numOfPillsDispensed;
+
+ // Creating a state  enum  to track when different sensors are activated/ when to wait for them
+
+ enum State{
+   WAITING_FOR_SENSOR1, // This is before any of the sensors detect a mag field
+   WAITING_FOR_SENSOR2 // This is for when the  first sensor detects  a mg field
+ };
+
+ State currentState = WAITING_FOR_SENSOR1;
 
 
  void setup(){
@@ -18,17 +32,35 @@
  }
 
  void loop(){
-    // Using the turnComplteted function , report how many supplement is dispensed.
-
+   if (turnCompleted() == true){
+      numOfPillsDispensed = numOfPillsDispensed +1;
+      Serial.Print("You have ");
+      Serial.Print(numOfPillsDispensed);
+      serial.Print (" left before refill");
+   }
+   
  }
 
  // Function to tell when a sucessfull turn in complete
- // Sensor 1 and Sensor 2 ould be at fixed location, and the rotary would go through sensor 1 before sensor 2
+ // Sensor 1 and Sensor 2 would be at fixed location, and the rotary would go through sensor 1 before sensor 2
 
-bool turnCompleted( int sensor1, int sensor2){
+bool turnCompleted(){
+ // Sensor is automatically starts not detecting anything, and changes state when sensor 1 detects
+ if (currentState == WAITING_FOR_SENSOR1){ 
+   if( digitalRead(hallEffect1) == LOW){
+      currentState =  WAITING_FOR_SENSOR2;
+   }
+ }
 
-// If sensor 1 is triggered, check if sensor 2 is triggered, if it's sensor 2 is triggered after sensor 1, return that turn was completed
-// If sensor 2 isn't triggered after sensor do nothing, 
-// Add soemthing for when the user turns past sensor 1 and then turns back to starting point 
+ // System State is at WAITING_FOR_SENSOR2, check the second sensor for it's activation
+ if (currentState == WAITING_FOR_SENSOR2)
+ {
+   if(digitalRead(hallEffect2)== LOW){
+      currentState = WAITING_FOR_SENSOR1; // Resets the state back to before sensor1 is detected
+      return true; // Returns true for the boolean
+     }
+ }
+  return false;
+
 }
 
